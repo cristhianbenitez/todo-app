70222ff7dc600d47ad532be9816c071a830a6649
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-11-16 19:17:03 -0500

    feat:add new task. Close #1

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 ## [devchallenges.io](https://devchallenges.io/challenges/hH6PbOHBdPm6otzw2De5)
 
 User story: I can add a new task
+
 User story: I can complete a task
+
 User story: I can toggle between All, Active and Completed
+
 User story: I can remove one or all tasks under the Completed tab
+
 User story (optional): Store the data in local storage that when I refresh the page I can still see my progress
--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = React.useState('');
+  const [tasks, setTasks] = React.useState([]);
+
+  const addNew = (task) => {
+    task.id = Math.round(Math.random() * 10000);
+    setTasks(tasks.concat(task));
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addNew({ text: text, isActive: false, isCompleted: false });
+
+    setText('');
+  };
+
   return (
-    <div className="App">
-      <h1>#todo</h1>
+    <div className="todo-app">
+      <div className="todo-app__container">
+        <h1 className="todo-app__heading">#todo</h1>
+
+        <nav className="todo-app__navbar">
+          <ul className="todo-app__navbar__menu">
+            <li>All</li>
+            <li>Active</li>
+            <li>Completed</li>
+          </ul>
+        </nav>
+
+        <section>
+          <form className="todo-app__searchbox" onSubmit={handleSubmit}>
+            <div className="todo-app__searchbox__input">
+              <input
+                type="text"
+                placeholder="add details"
+                onChange={handleChange}
+                value={text}
+              />
+            </div>
+
+            <button className='todo-app__searchbox__btn"' type="submit">
+              Add
+            </button>
+          </form>
+
+          <ul className="todo-app__todo-list">
+            {tasks.map((task) => (
+              <li key={task.id}>{task.text}</li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
